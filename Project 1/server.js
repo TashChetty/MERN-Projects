6ddeaeced20de87f1e2252c72ce099fdd2172776
TashChetty <natasha.chetty2@gmail.com>
2024-01-12 20:57:47 +0200

    Project 1

--- Project 1/server.js
+++ Project 1/server.js
@@ -3,8 +3,8 @@ const app = express();
 const path = require ('path');
 const PORT = process.env.PORT || 3500;
 
-app.use('/', express.static (path.join (__dirname, "/public")));
-
+app.use('/', express.static (path.join (__dirname, "public")));
+app.use(express.json())
 app.use('/', require('./routes/root'));
 
 app.all('*', (req,res) => {
