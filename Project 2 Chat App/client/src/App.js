8f818964a46bdf23b81dcd569eb2f21755f7f953
TashChetty <natasha.chetty2@gmail.com>
2024-01-16 14:53:05 +0200

    Project 2 Chat App with Socket io

--- Project 2 Chat App/client/src/App.js
+++ Project 2 Chat App/client/src/App.js
@@ -1,9 +1,12 @@
+import Button from '@mui/material-next/Button';
 
 
 function App() {
   return (
     <div>
       Hello Socket
+      <Button variant = "text" >Text</Button>
+
     </div>
   );
 }
