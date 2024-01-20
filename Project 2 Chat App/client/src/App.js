4589d2a20bac01285d322a3545745cac52bc7d1f
TashChetty <natasha.chetty2@gmail.com>
2024-01-20 21:10:45 +0200

    Project 2

--- Project 2 Chat App/client/src/App.js
+++ Project 2 Chat App/client/src/App.js
@@ -1,19 +1,33 @@
 import Button from '@mui/material-next/Button';
+import  TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
 import { useEffect, useState } from 'react';
 import {io} from 'socket.io-client';
 
 function App() {
 const [socket, setSocket] = useState(null);
-  
+   
   useEffect(() => {
     setSocket(io('http://localhost:4000'))
   }, []);
 
+  function handleForm(e){
+    e.preventDefault();
+  }
+
   return (
     <div>
-      Hello Socket
-      <Button variant = "text" >Text</Button>
-
+      
+      <Box cpmponent = "form"  onSubmit={handleForm}>
+      <TextField 
+      id ="standard-basic" 
+      size="small"
+      label="Standard" 
+      variant = "standard" 
+      
+      />
+      <Button variant = "text"  type = "submit">Send</Button>
+      </Box>
     </div>
   );
 }
