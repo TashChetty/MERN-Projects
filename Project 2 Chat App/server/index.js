012eb7521682d844b572c6c1c9e2263e88277cca
TashChetty <natasha.chetty2@gmail.com>
2024-01-21 23:18:56 +0200

    Project 2 Chat App

--- Project 2 Chat App/server/index.js
+++ Project 2 Chat App/server/index.js
@@ -1,29 +1,46 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
-import path from "path";
-import { fileURLToPath } from 'url';
+
 
 const app = express();
-const httpServer = http.createServer(app);
 const PORT = 4000;
+
+const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
     cors: {
-      origin: ["https://localhost:3000"], 
-    }
-  });
+        origin: ["http://localhost:3000"],
+    },
+});
 
+import path from "path";
+import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
+   // res.json({ data: "hello world from socket" });
+   res.sendFile(__dirname + "/index.html");
 });
 
-io.on("connection", (socket) => {
-    console.log("Connection is ready");
+io.on('connection', (socket) => {
+    //console.log('Connection is ready');
+    socket.on("send-message", (data) => {
+        socket.broadcast.emit("message-from-server", data);
+    });
+
+    socket.on("typing-started", () => {
+        socket.broadcast.emit("typing-started-from-server");
+    });
+
+    socket.on('disconnect', (socket) => {
+        console.log('User left.');
+    //  socket.on("send-message", (data) => {
+    //    socket.emit("message-from-server", data);
+    //})
+    });
 });
 
 httpServer.listen(PORT, () => {
-    console.log("Listening on port http://localhost:4000");
+    console.log("Server is running at http://localhost:4000")
 });
